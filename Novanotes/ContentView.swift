2e4e0266e24c710c6fab4319a296cfe83f766110
phuriungung <puriung08@gmail.com>
2023-09-06 19:11:35 +0700

    Initial Commit

---
+++ Novanotes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Novanotes
+//
+//  Created by minto on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Novanotes/NovanotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovanotesApp.swift
+//  Novanotes
+//
+//  Created by minto on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct NovanotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
