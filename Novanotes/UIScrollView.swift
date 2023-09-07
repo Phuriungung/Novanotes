beff5344c03193ca2656b731a123e44437bf69d6
phuriungung <puriung08@gmail.com>
2023-09-07 08:18:15 +0700

    add UIViewRepresentable for UIScrollView

---
+++ Novanotes/UIScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  UIScrollView.swift
+//  Novanotes
+//
+//  Created by minto on 7/9/2566 BE.
+//
+
+import SwiftUI
+
+struct RepresentUIScrollView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIScrollView {
+        UIScrollView()
+    }
+    
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIScrollView
+    
+}
