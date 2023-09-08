fa831acfdb7e3cd508eabca97910e6f31bbc0e7a
phuriungung <puriung08@gmail.com>
2023-09-08 17:25:50 +0700

    it shows file url but doesn't show pdfview

--- pdfViewer/ContentView.swift
+++ pdfViewer/ContentView.swift
@@ -1,55 +1,73 @@
-//
-//  ContentView.swift
-//  pdfViewer
-//
-//  Created by minto on 8/9/2566 BE.
-//
-
 import SwiftUI
 import PDFKit
-
-
-@main
-struct PDFImporterApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
-
-struct ContentView: View {
-    @State private var isImportingPDF = false
-
-    var body: some View {
-        NavigationView {
-            List {
-                // List of imported PDFs here
-            }
-            .navigationTitle("PDF Viewer")
-
-            Button("Import PDF") {
-                isImportingPDF.toggle()
-            }
-            .background(Color.black)
-            .fileImporter(
-                isPresented: $isImportingPDF,
-                allowedContentTypes: [.pdf]
-            ) { result in
-                // Handle the imported PDF here
-                // Display the confirmation dialog and import the document
-            }
-        }
-        .onOpenURL { url in
-            // Handle incoming URLs (PDFs) shared with the app
-            // Display the confirmation dialog and import the document
-        }
-    }
-}
-
+import UIKit
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PDFKitView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: UIViewRepresentableContext<PDFKitView>) -> PDFView {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        return pdfView
+    }
+    
+    func updateUIView(_ uiView: PDFView, context: UIViewRepresentableContext<PDFKitView>) {
+        // Leave this empty as we don't need to update the PDF
+    }
+}
+
+struct ContentView: View {
+    @State private var selectedPDFUrl: URL? // Store the selected PDF URL here
+    
+    @State var imported = false
+    @State var fileUrl: URL?
+    
+    var body: some View {
+        VStack {
+            
+            Button(action: {imported.toggle()}, label: {
+                Text("Import file")
+            })
+            if let theUrl = fileUrl {
+                Text("file url is \(theUrl.absoluteString)")
+            }
+        }
+        .fileImporter(isPresented: $imported, allowedContentTypes: [.pdf]) { res in
+            do {
+                fileUrl = try res.get()
+                print("---> fileUrl: \(fileUrl)")
+            } catch{
+                print ("error reading: \(error.localizedDescription)")
+            }
+            
+            
+            
+            Image(systemName: "doc.viewfinder")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text("PDF Viewer")
+                .foregroundColor(.accentColor)
+            
+            if let pdfUrl = selectedPDFUrl { // Check if a PDF has been selected
+                PDFKitView(url: fileUrl!)
+            }
+        }
+        .padding()
+    }
+}
+
+@main
+struct PDFViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
 }
